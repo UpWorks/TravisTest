2faf4696b6c0de2ef392442eadf252c7716656a0
Michael Updegraff <michael.updegraff@gmail.com>
2016-05-08 21:07:26 -0700

    added -o stricthostkeychecking=no

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -17,15 +17,10 @@ echo "*        Signing          *"
 echo "***************************"
 xcrun -log -v -sdk iphoneos9.2 PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
-echo "$OUTPUTDIR/$APP_NAME.app.dSYM.zip"
-ls -al "$OUTPUTDIR/$APP_NAME.app.dSYM.zip"
-echo "$OUTPUTDIR/$APP_NAME.app"
-ls -al "$OUTPUTDIR/$APP_NAME.app"
-
 zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
 export SSHPASS="$DEPLOY_PASS"
-sshpass -e -o sftp "$DEPLOY_USER"@"$DEPLOY_HOST":"$DEPLOY_PATH" <<< $'put "$OUTPUTDIR/$APP_NAME.app.dSYM.zip"'
+sshpass -e sftp -o stricthostkeychecking=no "$DEPLOY_USER"@"$DEPLOY_HOST":"$DEPLOY_PATH" <<< $'put "$OUTPUTDIR/$APP_NAME.app.dSYM.zip"'
