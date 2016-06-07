57f8be9abbb28ad8ea26768b9e74cc3186a37990
Michael Updegraff <michael.updegraff@gmail.com>
2016-06-06 18:23:11 -0700

    Re-added signing

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -15,7 +15,7 @@ OUTPUTDIR="$PWD/build/Release-iphoneos"
 echo "***************************"
 echo "*        Signing          *"
 echo "***************************"
-xcrun -log -v -sdk iphoneos9.2 PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -embed "$PROVISIONING_PROFILE"
+xcrun -log -v -sdk iphoneos9.2 PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
